77885d0e5e2d5809f7a36a4e6aea036a2b9baff3
jam4fox99 <jam4fox88@gmail.com>
2023-03-17 09:45:10 -0700

    Initial Commit

---
+++ Swift UI Camera Example/Swift_UI_Camera_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_Camera_ExampleApp.swift
+//  Swift UI Camera Example
+//
+//  Created by Jake Milken on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_Camera_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
